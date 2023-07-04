8d5e6d9fc2e852f187f503ba1d23901001bf84af
Daniel <38507909+kodan50@users.noreply.github.com>
2023-07-03 20:51:11 -0700

    Create CopyWindowsSpotlightImages.bat
    I've decided to stop requiring the user to split the GitAdmin() code, because it's just not worth the effort.

---
+++ Windows/CopyWindowsSpotlightImages.bat
@@ -0,0 +1,52 @@
+@echo off
+pushd "%~dp0"
+
+:: We run as an admin so we can access everyone's user folders.
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~0"
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+
+  if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+ 
+cls
+
+if not exist cache mkdir cache
+
+:: This should scan all drive letters for a user folder, and will retrieve cache files.
+for %%a in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+ if exist "%%a:\Users\" (
+  for /D %%G in ("%%a:\Users\*") do (
+   echo n | copy /-y "%%G\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" .\cache
+  )
+ )
+)
+
+goto :EOF
