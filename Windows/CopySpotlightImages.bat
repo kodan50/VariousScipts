8856030c491dc4b42b65222302404d1afd27774b
Daniel <38507909+kodan50@users.noreply.github.com>
2023-07-03 20:55:20 -0700

    Update CopySpotlightImages.bat
    If your Windows installation just happens to be on another drive letter, this will scan them all and copy your spotlight cache. As a side effect, it will also copy the users folder for any other drive attached with a Windows directory.

--- Windows/CopySpotlightImages.bat
+++ Windows/CopySpotlightImages.bat
@@ -1,7 +1,5 @@
 @echo off
-
-:: When running off a network location, you get a bunch of error messages about running scripts off network locations, blah blah. We delt with it. Let's CLear Screen.
-cls
+pushd "%~dp0"
 
 :: We run as an admin so we can access everyone's user folders.
 :init
@@ -37,23 +35,17 @@ cls
 :gotPrivileges
  setlocal & cd /d %~dp0
  if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
-
+ 
 cls
 
-::We need to perform some directory handling. I run this off a network drive, so this makes that possible.
-::This works when run off a flash drive as well, so this should be a "one size fits all" type thing.
-pushd "%~dp0"
-
-::We are going to create the folder if it doesn't already exist.
 if not exist cache mkdir cache
 
-::Now we can copy the files over.
-for /D %%G in ("C:\Users\*") do (
-    echo n | copy /-y "%%G\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" .\cache
+for %%a in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+ if exist "%%a:\Users\" (
+  for /D %%G in ("%%a:\Users\*") do (
+   echo n | copy /-y "%%G\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" .\cache
+  )
+ )
 )
- 
-::We want to unmount the network folder cleanly. This should do the trick. I don't know if it is needed or not, but it was suggested.
-popd
 
-:exit
-pause
+goto :EOF
